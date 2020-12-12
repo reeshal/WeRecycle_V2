71703cd799c7d1637756c983e863d0d198c325cf
reeshal <rreeshal@gmail.com>
2020-12-12 20:38:08 +0400

    filled navbar

--- websiteWR/src/app/Shared/shared.module.ts
+++ websiteWR/src/app/Shared/shared.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SpinnerComponent } from './spinner/spinner.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [SpinnerComponent, NavbarComponent],
+  declarations: [NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
-    SpinnerComponent,
     NavbarComponent
   ]
 })
