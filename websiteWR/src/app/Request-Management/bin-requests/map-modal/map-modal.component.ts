c4434c209581e6cdc204a0dc2b3552e1dd8d435e
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2020-12-29 15:13:46 +0400

    fix map

--- websiteWR/src/app/Request-Management/bin-requests/map-modal/map-modal.component.ts
+++ websiteWR/src/app/Request-Management/bin-requests/map-modal/map-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
@@ -6,32 +6,30 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
-  styleUrls: ['./map-modal.component.css']
+  styleUrls: ['./map-modal.component.css'],
 })
 export class MapModalComponent implements OnInit {
-
   @Input() latitude: number = -20; //default initialised
   @Input() longitude: number = 57; //default initialised
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    console.log('hello')
-    console.log([this.latitude, this.longitude])
+    console.log('hello');
+    console.log([this.latitude, this.longitude]);
     const map = new mapboxgl.Map({
       container: 'mapbox',
       accessToken: environment.mapbox.accessToken,
       style: 'mapbox://styles/mapbox/streets-v11',
       zoom: 10,
-      center: [-20,57],
+      center: [this.longitude, this.latitude],
     });
     const el = document.createElement('img');
     el.className = 'marker';
     el.src = `assets/images/bin-3.png`;
     el.style.width = '40px';
     new mapboxgl.Marker(el)
-      .setLngLat([-20,57])
+      .setLngLat([this.longitude, this.latitude])
       .addTo(map);
   }
-
 }
