ba8f4815a3aa5bea4b14fd4c247dabd861fdcbf2
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2021-01-21 17:44:02 +0400

    change env

--- websiteWR/src/environments/environment.prod.ts
+++ websiteWR/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1IjoiaXNmYWFxZyIsImEiOiJja2dvdzdjeWswNGdsMnF0ZHc2dWt5bGN1In0.OArOPU3qPBbZft81qCm6Tw',
+  },
+  // apiURL: 'http://localhost:51433/api',
+  apiURL: 'https://dcbackend.conveyor.cloud/api',
 };
