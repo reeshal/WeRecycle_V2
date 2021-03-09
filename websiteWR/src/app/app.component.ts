8de887d96564d0586b12a84c69e76d258b6860ad
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2021-03-09 18:55:14 +0400

    hide navbar on homepage

--- websiteWR/src/app/app.component.ts
+++ websiteWR/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'We Recycle';
+  currentRoute: string;
+
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.currentRoute = val.url;
+      }
+    });
+  }
 }
